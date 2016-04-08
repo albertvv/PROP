43d2ce57316db228f1cb46f457ccac0bec1699b7
albertvv <albertvalvila1@gmail.com>
2016-04-08 19:45:09 +0200

    hey

--- src/com/albertval/Main.java
+++ src/com/albertval/Main.java
@@ -3,6 +3,6 @@ package com.albertval;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("hey");
+        System.out.print("hey gent ");
     }
 }
---
+++ src/com/albertval/Query.java
@@ -0,0 +1,8 @@
+package com.albertval;
+
+/**
+ * Created by Albert on 08/04/2016.
+ */
+public class Query {
+    private String path;
+}
