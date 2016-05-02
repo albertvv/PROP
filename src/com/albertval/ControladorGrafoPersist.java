a435ed06b765f8d22f04195c58d17bbe19896393
alvarhc2 <alvarhc2@gmail.com>
2016-05-02 19:37:42 +0200

    Update ControladorGrafoPersist.java

--- src/com/albertval/ControladorGrafoPersist.java
+++ src/com/albertval/ControladorGrafoPersist.java
@@ -24,7 +24,7 @@ public class ControladorGrafoPersist {
 
     public ControladorGrafoPersist() {
         //Direccions dels fitxers continguts a DBLP
-        String dir = "C:\\Users\\Àlvar\\IdeaProjects\\LLegir\\src\\prop\\alvarHernandez\\DBLP_four_area\\";
+        String dir = "src\\prop\\alvarHernandez\\DBLP_four_area\\";
         this.paper = dir + "paper.txt";
         this.author = dir + "author.txt";
         this.conf = dir + "conf.txt";
